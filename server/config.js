9bfe9945468cb2320ebd32b7a9006dc578af7898
Param Shah <paramshah09@gmail.com>
2021-05-01 03:32:40 -0700

    Adds an OR statement for URL_PORT in config.js to select port from the EB environment.
    Why: The server deployment was failing on EB because it was listening on port 3000 when the EB
    environment was configured to route requests to 80 (specified by process.env.PORT)

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const config = {
   ROOT_DIR: __dirname,
-  URL_PORT: 3000,
+  URL_PORT: process.env.PORT || 3000,
   URL_PATH: 'http://localhost',
   BASE_VERSION: '',
   CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
