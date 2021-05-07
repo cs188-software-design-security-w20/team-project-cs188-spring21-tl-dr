0cabd341815d1adbbff5753075e80f4f00136ada
Linus <linuslyt@gmail.com>
2021-05-07 13:07:06 -0700

    Client - Add Google Sign In button, fetch stub for /login

---
+++ client/src/lib/fetch.js
@@ -0,0 +1,17 @@
+import { SERVER_URL } from '../constants';
+
+export async function signupOrLoginUser(id_token) {
+  const loginEndpoint = `${SERVER_URL}/login`;
+  // Check if user exists at /user endpoint
+  console.log(id_token);
+  const res = await fetch(loginEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: { 
+      id_token
+    },
+  });
+  return await res.json(); // or res.text() depending on backend implementation
+}
