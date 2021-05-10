f7f40a6206b11e2378159285b390369d6fa051e4
jonathanwong113 <jonathanwong113@gmail.com>
2021-05-11 01:38:18 +0800

    Added jwt

--- server/services/AuthService.js
+++ server/services/AuthService.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 const { db, Summary, User } = require('../db/psql.js');
-
+const jwt = require('jsonwebtoken');
 
 /**
 * Log in user. New users will be signed up then logged in.
@@ -11,7 +11,8 @@ const { db, Summary, User } = require('../db/psql.js');
 * returns Error
 * */
 
-const CLIENT_ID = '944387746626-hvgrqhj7ua1vlqsv6u0scddv0ac2djq0.apps.googleusercontent.com';
+const CLIENT_ID = '944387746626-hvgrqhj7ua1vlqsv6u0scddv0ac2djq0.apps.googleusercontent.com'; //TODO move to env file
+const SECRET = '343A58E25EA3F43698FE4BAE5ECA2'; // TODO generate + move to env file
 
 const loginPOST = ({ loginRequest }) => new Promise(
   async (resolve, reject) => {
@@ -35,13 +36,15 @@ const loginPOST = ({ loginRequest }) => new Promise(
         const email = payload['email'];
         const picture = payload['picture'];
         await User.findOrCreate({ where: { id: userid, firstName: firstName, lastName: lastName, email: email, image: picture }}); 
-      
+        
+        var token = jwt.sign({user: userid}, SECRET, {expiresIn: '1h'});
         resolve(Service.successResponse(
-          userid
+          token,
+          200
         ));
       }
       var token_error = (e) => reject(Service.rejectResponse(
-       e || ' Error Validating Token',
+       'An error occurred while validating the token',
        401
       ));
 
