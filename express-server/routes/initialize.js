d7bda98b8d9dba4ae620721c44356ef6c51120fa
Linus <linuslyt@gmail.com>
2021-05-29 19:18:39 -0700

    Remove legacy server

--- express-server/routes/initialize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var express = require('express');
-var router = express.Router();
-var initializeController = require('../controllers/Initialize');
-
-// set tag
-router.get('/initialize', initializeController.init);
-
-module.exports = router;
